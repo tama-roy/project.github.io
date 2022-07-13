1344dad76f36ffbc6b138e76604945e5cd50c037
tama-roy <108477463+tama-roy@users.noreply.github.com>
2022-07-13 21:25:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(
-  ".content-container,.footer,.navbar,.navbar-container"
-);
+$(document).ready(function(){
+  document.getElementById("#comment").click(function(){
+var input = $('#input').val(" ");
+document.getElementById(".box").append(input+ "<br>");
+document.getElementById("#input").val(" ");
+document.getElementById(".box-container").slidedown();
+});
 
-ball.addEventListener("click", () => {
-  items.forEach((item) => {
-    item.classList.toggle("active");
-  });
-  ball.classList.toggle("active");
+document.getElementById("#cancel").click(function(){
+document.getElementById("#input").val(" ");
+});
+
+document.getElementById("#delete").click(function(){
+
+document.getElementById("#box").text(" ");
+document.getElementById(".box-container").slideup();
+});
 });
