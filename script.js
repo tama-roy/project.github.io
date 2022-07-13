4ee2f6089c4b9b2b32e4f51a9f9e07cf685482c0
tama-roy <108477463+tama-roy@users.noreply.github.com>
2022-07-13 21:38:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-$(document).ready(function(){
-  document.getElementById("#comment").click(function(){
-var input = $('#input').val(" ");
-document.getElementById(".box").append(input+ "<br>");
-document.getElementById("#input").val(" ");
-document.getElementById(".box-container").slidedown();
-});
-
-document.getElementById("#cancel").click(function(){
-document.getElementById("#input").val(" ");
-});
-
-document.getElementById("#delete").click(function(){
-
-document.getElementById("#box").text(" ");
-document.getElementById(".box-container").slideup();
-});
-});
-
 const ball = document.querySelector(".toggle-ball");
 const items = document.querySelectorAll(
   ".content-container,.footer,.navbar,.navbar-container"
